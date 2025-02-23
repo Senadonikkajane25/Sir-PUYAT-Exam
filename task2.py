dbee151f6116dcce2f6ca22763c6b3d7c3f0477f
Senadonikkajane25 <senadonikkajane@gmail.com>
2025-02-23 18:08:29 +0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -8,9 +8,12 @@ class Student:
         print(f"Hello, my name is {self.name}. I am {self.age} years old and I am studying {self.course}.")
 
 student1 = Student("Nikka Jane Senado", 21, "DIP-Information Technology")
-student2 = Student("Jane Senado", 22, "DIP-Information Technology")
-student3 = Student("Niks Senado", 20, "DIP-Information Technology")
+student2 = Student("Jane Senado", 22, "DIP-Culinary")
+student3 = Student("Niks Senado", 20, "DIP-Hotel Management")
 
+print("Student 1:")
 student1.introduce()
+print("Student 2:")
 student2.introduce()
+print("Student 3:")
 student3.introduce()
