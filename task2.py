7daec405af2e8def4f3950f5fdc9a6d767c7f357
Senadonikkajane25 <senadonikkajane@gmail.com>
2025-02-23 17:54:42 +0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,5 +1,4 @@
 # task2.py
-
 class Student:
     def __init__(self, name, age, course):
         self.name = name
@@ -9,12 +8,10 @@ class Student:
     def introduce(self):
         print(f"Hello, my name is {self.name}. I am {self.age} years old and I am studying {self.course}.")
 
-# Create three student objects
-student1 = Student("Nikka Jane Senado", 20, "DIP-Information Technology 2")
-student2 = Student("Charmaine Villanueva", 22, "DIP-Information Technology 2")
-student3 = Student("Jhen Senado", 21, "DIP-Information Technology 2")
+student1 = Student("Nikka Jane Senado", 21, "DIP-Information Technology")
+student2 = Student("Jane Senado", 22, "DIP-Information Technology")
+student3 = Student("Niks Senado", 20, "DIP-Information Technology")
 
-# Call the introduce() method for each student
 student1.introduce()
 student2.introduce()
 student3.introduce()
